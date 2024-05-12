6213b4c8a7e4ce525d570cc3afcd03663c07bd91
Peter Cardillo <pcardillo@streamingturtles.com>
2024-05-12 13:19:04 -0400

    added multer.js file, more to edit in the feature/file-upload branch

--- config/multer.js
+++ config/multer.js
@@ -1,10 +1,14 @@
 
-
+const path = require('path');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './public/images/thumbnails');
+  },
+  filename: (req, file, cb) => {
+    const fileName = `avatar-${Date.now()}${path.extname(file.originalname)}`;
+    cb(null, fileName);
   },
 
 });
